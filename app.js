83925ed9effb1fff8bb7d190c63ae748ef9ea96e
Kritika Srivastava <kritikasri27@gmail.com>
2023-05-28 06:14:54 +0530

    Committing algo 1

---
+++ app.js
@@ -0,0 +1,10 @@
+//Day 1
+//Move the first letter of each word of the sentence to last and add "ay"
+//slice,charAt,map,split,join
+let str="We love javascript";
+
+let temp=str.split(" ").map(el=>{
+    return el.slice(1)+ el.charAt(0)+ "ay"
+}).join(" ");
+
+console.log(temp);//eWay ovelay avascriptjay
