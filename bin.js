8e729699b4543f47083c712947de5de093fd2225
Kritika Srivastava <kritikasri27@gmail.com>
2023-05-31 10:24:43 +0530

    Converting binary to decimal

---
+++ bin.js
@@ -0,0 +1,10 @@
+
+//Have the function BinaryConverter(str) return the decimal form of the binary value. 
+//For example: if 101 is passed return 5, or if 1000 is passed return 8.
+function BinaryConverter(str) { 
+
+    // code goes here  
+    return parseInt(str,2); 
+  
+  }
+  
