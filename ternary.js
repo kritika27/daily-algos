268c1f04e67d8b1353236f155d6726c5d53e97c4
Kritika Srivastava <kritikasri27@gmail.com>
2023-06-27 19:48:22 +0530

    Ternary Converter

---
+++ ternary.js
@@ -0,0 +1,7 @@
+// Have the function TernaryConverter(num) take the num parameter being passed, which will always be a positive integer, and convert it into a ternary representation. For example: if num is 12 then your program should return 110.
+
+//Solution:
+
+let num=12;
+console.log(Number(num.toString(3)));//110
+  
