3a5acdb94e6266a115f0d6665243f3b05d5eaaf5
Kritika Srivastava <kritikasri27@gmail.com>
2023-07-04 17:39:05 +0530

    gcf

---
+++ gcf.js
@@ -0,0 +1,18 @@
+// Have the function GCF(arr) take the array of numbers stored in arr which will always contain only two positive integers, and return the greatest common factor of them. For example: if arr is [45, 12] then your program should return 3. There will always be two elements in the array and they will be positive integers.
+
+function GCF(arr) { 
+
+    // code goes here  
+    let gcf=0;
+    for (let i = 1; i <= arr[0] && i <= arr[1]; i++) {
+  
+      // check if is factor of both integers
+      if( arr[0] % i === 0 && arr[1] % i === 0) {
+          gcf = i;
+      }
+  }
+  return gcf;
+  
+  
+  }
+     
